4b23b486299f9fdc8cc9d02b429f2fd80b3b13cb
emlai <emil.laine@cs.helsinki.fi>
2016-10-25 17:43:17 +0300

    Use custom swift-toml version that compiles out of the box

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
     ],
     dependencies: [
         .Package(url: "https://github.com/emlai/CSDL2.swift.git", majorVersion: 1),
-        .Package(url: "https://github.com/jdfergason/swift-toml.git", Version(0, 4, 0)),
+        .Package(url: "https://github.com/emlai/swift-toml.git", "0.4.0+zenith"),
     ]
 )
